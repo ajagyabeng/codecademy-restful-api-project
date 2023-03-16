d48d57054c9d58fef98f0c23f324222d2c8e2ae6
ajagyabeng <ajagyabeng@gmail.com>
2023-03-16 19:53:03 +0000

    Upadate error messages

--- api/common/errors.py
+++ api/common/errors.py
@@ -6,17 +6,17 @@ class UserErrors():
         abort(404, message=f"User with ID: {pk} doesn't exist. Check user ID.")
 
     def abort_if_user_wasnt_added():
-        abort(500, message=f"User wasn't added to database.")
+        abort(500, message=f"Unsuccessful user creation.")
 
 
 class VenueErrors():
     def abort_if_user_doesnt_exist(pk):
-        abort(404, message=f"User with ID: {pk} doesn't exist")
+        abort(404, message=f"Venue with ID: {pk} doesn't exist")
 
 
 class PhotoErrors():
-    def abort_if_venue_doesnt_exist(venueId):
-        abort(404, message=f"Venue with ID: {venueId} doesn't exist")
+    def abort_if_venue_has_no_photos(venueId):
+        abort(404, message=f"Venue with ID: {venueId} doesn't have any photos")
 
     def abort_if_photo_doesnt_exist(pk):
         abort(
