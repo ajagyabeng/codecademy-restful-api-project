1b64900b339ea35367fd4536fb5066ee3bb67fc7
ajagyabeng <ajagyabeng@gmail.com>
2023-03-17 16:32:22 +0000

    Import auth, add csrf protection

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_wtf.csrf import CSRFProtect
 
 from dotenv import find_dotenv, load_dotenv
 import os
@@ -9,13 +10,19 @@ from .models import setup_db
 
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
+csrf = CSRFProtect()
 
 
 def create_app():
     """Creates and initialize app with modules"""
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.getenv("APP_SECRET_KEY")
-    CORS(app)
+    # CORS(app)
+    # csrf.init_app(app)
+
+    from .controller.auth import auth
+    app.register_blueprint(auth, url_prefix="/")
+
     setup_db(app)
 
     return app
